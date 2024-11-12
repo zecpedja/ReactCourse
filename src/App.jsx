ee9f17430e207fce5e1930ad38aa1f9214037097
zecpedja <127168701+zecpedja@users.noreply.github.com>
2024-11-12 07:31:52 +0100

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import './App.css'
 
 
-  function WelcomeMessage(name) {
-    return <h1>Hello {name}</h1>
-  }
-
   function App() {
+  const name = "John Doe";
+  const profession = "Full Stack Developer";
+  const projects = [
+    { name: "Project 1", description: "Description of Project 1", link: "#" },
+    { name: "Project 2", description: "Description of Project 2" , link: "#"}]
   return (
-    <div>
-    {WelcomeMessage('Alice')}
-    <h1>Hello Bob</h1>
+    <div className="App">
+      <header className='header'>
+        <h1>{name}</h1>
+        <p>{profession}</p>
+        <nav>
+          <a href='#about'>About</a>
+          <a href='#projects'>Projects</a>
+          <a href='#contact'>Contact</a>
+        </nav>
+      </header>
+      {/* about section */}
+      <section id='projects' className='about-section'>
+        <h2>About Me</h2>
+        <p>Hello! I'm {name}, a {profession}.</p>
+      </section>
+      {/* projects section */}
     </div>
+  
   )
 }
-export default App
+export default App;
